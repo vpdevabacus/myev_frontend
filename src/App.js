eec7102846f82645aef4374fba2288e37520450f
vpdevabacus <developer@abacusmail.in>
2023-12-02 11:26:26 +0000

    add new files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div>
+     <h1 className="text-2xl font-bold border-2 text-[#e50914] w-30">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
