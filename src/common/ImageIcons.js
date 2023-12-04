8a6449c5915d3c31153b0926d643e2c4b76a3c57
vpdevabacus <developer@abacusmail.in>
2023-12-04 18:05:04 +0530

    files

--- src/common/ImageIcons.js
+++ src/common/ImageIcons.js
@@ -1,8 +1,16 @@
 import myEV_logo from "../assets/Images/myEV_logo.png";
+import bannerimage from "../assets/Images/bannerimage.png";
+import car from "../assets/Images/car.png";
+
+
+
+
 
 
 const ImageIcons = {
     myEV_logo,
+    bannerimage,
+    car
 }
 
 export default ImageIcons;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "../components/header/Header";
+import Banner from "../components/home/Banner";
 
 const Home = () => {
     return (
         <>
             <Header />
+            <Banner/>
+
         </>
     )
 };
