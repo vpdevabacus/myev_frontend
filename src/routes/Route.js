a3242438c256b7e7c14db332e39638bfc4abc2f4
vpdevabacus <developer@abacusmail.in>
2023-12-07 11:29:32 +0530

    add favicon

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import {
-    Route,
-    createBrowserRouter,
-    createRoutesFromElements,
-} from "react-router-dom";
+    Route,createBrowserRouter,createRoutesFromElements,} from "react-router-dom";
 import Home from '../screens/Home';
+import About from '../components/about/About';
 
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Home />}>
+        <Route path="/About" element={<About/>}>
+        </Route>
         </Route>
     )
 );
