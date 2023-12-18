323143043141e9c75510a2f9b755be57ea648129
vpdevabacus <developer@abacusmail.in>
2023-12-18 10:11:26 +0530

    add files

--- src/common/ImageIcons.js
+++ src/common/ImageIcons.js
@@ -100,21 +100,16 @@ const ImageIcons = {
   whyUS,
   contact_banner,
   Rectangle1,
-<<<<<<< HEAD
   Aboutimage,
   percentage,
   vision,
   mission,
   pattern ,
-=======
->>>>>>> 85985d78d52c1ef411dc834857d835524286d993
   fastMachine,
   slowMachine,
   team_member_3,
   team_member_2,
-<<<<<<< HEAD
-  team_member_1
-=======
+  team_member_1,
   team_member_1,
   Aboutimage,
   percentage,
@@ -122,7 +117,6 @@ const ImageIcons = {
   mission,
   pattern
 
->>>>>>> 85985d78d52c1ef411dc834857d835524286d993
 
 }
 
