9e060ce7732be1ccd98f671cbcb6f90a4cbdbf90
vpdevabacus <developer@abacusmail.in>
2023-12-22 17:59:28 +0530

    added contact us

--- src/components/shared/InnerPageBanner.js
+++ src/components/shared/InnerPageBanner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const InnerPageBanner = ({ title = "", innerPageBanner ="" }) => {
     return (
-        <section className='py-[160px] bg-center bg-cover bg-no-repeat' style={{ backgroundImage: `url(${innerPageBanner})` }}>
+        <section className='md:py-[110px] max-md:py-[50px] bg-center bg-cover bg-no-repeat' style={{ backgroundImage: `url(${innerPageBanner})` }}>
             <div className='heading_tittle'>
                 {title && (
                     <h1 className="capitalize text-white text-center">{title}</h1>
