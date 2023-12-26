6ffde8a8e89b8f135714665301f1eeb2d2a1e9a3
vpdevabacus <developer@abacusmail.in>
2023-12-26 10:47:55 +0530

    add files

--- src/components/services/FAQs.js
+++ src/components/services/FAQs.js
@@ -1,136 +1,89 @@
-import React, { useState, useRef } from 'react';
+import React from "react";
 
+import ReactMapGL, {
+  Layer,
+  Marker,
+  NavigationControl,
+  Source,
+} from "react-map-gl";
+import { TOKEN } from "./Geocoder";
 
+// -26.475393195281754, 153.04416685709924
 
+const AppMap = ({
+  mapRef,
+  setNewPlace,
+  newPlace,
+  polygonCord,
+  layerColor,
+  viewport,
+  setViewport,
+}) => {
+  const handleAddClick = (e) => {
+    setNewPlace({
+      lat: e?.lngLat?.lat,
+      lng: e?.lngLat?.lng,
+    });
+  };
+  const geojson = {
+    type: "Feature",
+    geometry: {
+      type: "Polygon",
+      coordinates: [polygonCord],
+    },
+  };
 
+  const layerStyle = {
+    id: "maine",
+    type: "fill",
+    source: "maine", // reference the data source
+    layout: {},
+    paint: {
+      "fill-color": layerColor || "#0080ff", // blue color fill
+      "fill-opacity": 0.5,
+    },
+  };
+  // Add a black outline around the polygon.
+  const layerOutlineStyle = {
+    id: "outline",
+    type: "line",
+    source: "maine",
+    layout: {},
+    paint: {
+      "line-color": "#000",
+      "line-width": 3,
+    },
+  };
 
+  return (
+    <ReactMapGL
+      ref={mapRef}
+      mapboxAccessToken={TOKEN}
+      initialViewState={viewport}
+      onViewportChange={(viewport) => setViewport(viewport)}
+      mapStyle="mapbox://styles/dmsdesigns/clid0566a000y01q14exw4ruv"
+      onDblClick={handleAddClick}
+      transitionDuration="200"
+      attributionControl={true}
+    >
+      <Source id="my-data" type="geojson" data={geojson}>
+        <Layer {...layerOutlineStyle} />
+        <Layer {...layerStyle} />
+      </Source>
 
-const FAQs = () => {
-
-    const markers = [
-        { latitude: 37.7749, longitude: -122.4194 }, // San Francisco
-        { latitude: 34.0522, longitude: -118.2437 }, // Los Angeles
-        
-        { latitude: 40.7128, longitude: -74.0060 }, // New York
-    ];
-
-    const markersHtml = markers
-        .map((marker) => `<marker location="${marker.latitude},${marker.longitude}"></marker>`)
-        .join('');
-
-        const iframeHtml = `
-        <iframe
-          title="Google Map"
-          width="600"
-          height="450"
-          frameBorder="0"
-          style="border:0"
-          src="https://www.google.com/maps/embed/v1/view?key=&center=0,0&zoom=1&${markersHtml}"
-          allowfullscreen>
-        </iframe>
-      `;
-
-
-
-    const [activeTab, setActiveTab] = useState(null);
-    const [iconColor, setIconColor] = useState('#0B7132'); // Initial color
-
-
-    const handleClick = (id) => {
-        setActiveTab((prev) => (prev === id ? null : id));
-        setIconColor((prevColor) => (prevColor === '#0B7132' ? '#000000' : '#0B7132'));
-
-    };
-
-    const handleRotate = (id) => (activeTab === id ? '-rotate-180' : '');
-
-    const handleToggle = (id) =>
-        activeTab === id ? { maxHeight: '100%' } : { maxHeight: '0' };
-
-    const handleHeading = (id) =>
-        activeTab === id ? { backgroundColor: '#0B7132', borderRadius: '6px', color: '#fff' } : {};
-
-
-
-    const items = [
-        {
-            id: 1,
-            ques: 'Is myEV Point offering charge points at business/work premises?',
-            ans: "myEV Point offers end-to-end customized solutions for EV Charging needs at business/work premises."
-        },
-        {
-            id: 2,
-            ques: 'What about safety and maintenance?',
-            ans: "myEV Point Chargers comes with in-built multiple protection systems which significantly reduces risks related to electric safety at Commercial spaces & Offices. myEV Point also offers Annual Maintenance for almost all of our charger models."
-        },
-        {
-            id: 3,
-            ques: 'Whom do I contact in case of any problem while charging my car or with my charger?',
-            ans: "Please contact myEV Point customer care team for any problems with your charger which is supplied by myEV Point."
-        },
-        {
-            id: 4,
-            ques: 'Do I need my own charging cable?',
-            ans: "Almost all of the DC fast chargers have inbuilt connector cables. However, some chargers (typically some AC chargers) requires charging connector cable to be brought by the user."
-        },
-    ];
-
-    return (
-        <div className=" w-full mx-auto my-12 md:my-32 tracking-wide md:px-2 md:mt-20">
-            <h2 className="text-center text-3xl text-black font-semibold md:text-4xl lg:text-5xl mb-10 ">Frequently Asked Questions</h2>
-
-            <div className="container mx-auto grid gap-6 py-8 px-20 text-lg leading-6 text-gray-800 md:gap-8 md:grid-cols-2">
-                {items.map((item) => (
-                    <div key={item.id} className="space-y-3">
-                        <div
-                            className="relative transition-all duration-700 border rounded-md hover:shadow-2xl"
-                            onClick={() => handleClick(item.id)}
-                        >
-                            <div style={handleHeading(item.id)}
-                                className="w-full p-2 text-left cursor-pointer">
-                                <div className="flex items-center justify-between px-2">
-                                    <span className="tracking-wide text-lg sm:text-xl lg:text-2xl font-medium">{` ${item.ques}`}</span>
-                                    <span
-                                        className={`transition-transform duration-200 transform fill-current ${handleRotate(
-                                            item.id
-                                        )}`}
-                                    >
-                                        <svg
-                                            className="w-8 h-8 fill-current"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20"
-
-                                        >
-                                            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                                        </svg>
-                                    </span>
-                                </div>
-                            </div>
-
-                            <div
-                                style={handleToggle(item.id)}
-                                className="relative overflow-hidden transition-all duration-200 max-h-0"
-                            >
-                                <div className="px-6 py-4 text-black text-base lg:text-lg lg:text-xl font-normal">{`${item.ans}`}</div>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div>
-            <div dangerouslySetInnerHTML={{ __html: iframeHtml }} />
-            
-    {/* <iframe
-      title="Google Map"
-      width="600"
-      height="450"
-      frameBorder="0"
-      style="border:0"
-      src="https://www.google.com/maps/embed/v1/view?key=&center=0,0&zoom=1&${markersHtml}"
-      allowfullscreen>
-    </iframe> */}
-
-        </div>
-    );
+      {newPlace ? (
+        <Marker
+          latitude={newPlace?.lat}
+          longitude={newPlace?.lng}
+          draggable
+          onDragEnd={(e) =>
+            setNewPlace({ lng: e.lngLat.lng, lat: e.lngLat.lat })
+          }
+        />
+      ) : null}
+      <NavigationControl position="bottom-right" />
+    </ReactMapGL>
+  );
 };
 
-export default FAQs;
+export default AppMap;
